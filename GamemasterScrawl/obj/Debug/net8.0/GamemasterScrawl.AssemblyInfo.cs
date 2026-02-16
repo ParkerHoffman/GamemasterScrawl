07c0e2b956004287e931e29f4aa489fc51c5efeb
ParkerHoffman <132070255+ParkerHoffman@users.noreply.github.com>
2026-02-16 10:02:23 -0600

    Run

--- GamemasterScrawl/obj/Debug/net8.0/GamemasterScrawl.AssemblyInfo.cs
+++ GamemasterScrawl/obj/Debug/net8.0/GamemasterScrawl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fafb99acc43c082319e0f95029d60917aac69c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b6daff3e1f96f4bbd78041b25305ec69d88936")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
