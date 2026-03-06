0b991a73f586fa3090c9c0bbc10de87bb4f8e44c
Parker Hoffman <132070255+ParkerHoffman@users.noreply.github.com>
2026-03-06 14:30:00 -0600

    Ubunut Login Fix

--- GamemasterScrawl/obj/Debug/net8.0/GamemasterScrawl.AssemblyInfo.cs
+++ GamemasterScrawl/obj/Debug/net8.0/GamemasterScrawl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48fa183f0e6b1feb15738d9ffa2d3fada1462940")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02752f984007e610e3196dd797bca113b7cc095")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GamemasterScrawl/wwwroot/Components/js/login.js
+++ GamemasterScrawl/wwwroot/Components/js/login.js
@@ -138,8 +138,12 @@ var zgoPositive = RandomIntGen() >= 0;
 
 
 function AddOminousCube(container, appState){
-        appState.sceneSet.add({renderer: renderer, scene: scene})
+        
         const renderer = new THREE.WebGLRenderer();
+
+        appState.sceneSet.add({renderer: renderer, scene: scene})
+
+        
         renderer.setClearColor(0x000000, 0); // transparent background
 renderer.setSize(window.innerWidth, window.innerHeight);
 
