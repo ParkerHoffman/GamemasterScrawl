8f2a1cbed7a4493a0fbca73c8209bc9a8ed04175
Parker Hoffman <132070255+ParkerHoffman@users.noreply.github.com>
2026-03-08 23:11:56 -0500

    Added alternate click

--- GamemasterScrawl/obj/Debug/net8.0/GamemasterScrawl.AssemblyInfo.cs
+++ GamemasterScrawl/obj/Debug/net8.0/GamemasterScrawl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e668a03d5667603914b5f1353fc603d96e5c4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b504502f04466a6288c45699115549ee788de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
