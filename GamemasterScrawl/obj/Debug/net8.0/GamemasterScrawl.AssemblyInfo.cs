e976001a6098318753bb5a1c32b0cf3df68342fa
Parker Hoffman <132070255+ParkerHoffman@users.noreply.github.com>
2026-04-03 15:58:43 -0500

    Login also fixed

--- GamemasterScrawl/obj/Debug/net8.0/GamemasterScrawl.AssemblyInfo.cs
+++ GamemasterScrawl/obj/Debug/net8.0/GamemasterScrawl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ef3c279e349c00109b16dcb42f5189c7bd1052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc7d1174b45ef473605e1fedf0c51a6605e5a30")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamemasterScrawl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
